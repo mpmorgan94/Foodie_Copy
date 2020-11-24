a7e24b31abb1ba0115a7891bb7ff49f1341a0826
John Patranella <mpatranella@tamu.edu>
2020-11-24 17:59:33 -0600

    changed ip

--- gui/src/DatabaseHandler.js
+++ gui/src/DatabaseHandler.js
@@ -10,14 +10,14 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
   var credsVerified = false;
 
   function getUsers() {
-    fetch('http://localhost:3001/')
+    fetch('http://18.218.252.219:3001/')
       .then(response => {
         return response.text();
       })
   }
 
   async function userExist(username) {
-    await fetch(`http://localhost:3001/userExist`, {
+    await fetch(`http://18.218.252.219:3001/userExist`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -49,7 +49,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
     let ph = new PasswordHasher();
     let passwordHash = ph.hashPassword(password);
 
-    await fetch(`http://localhost:3001/verifyCreds`, {
+    await fetch(`http://18.218.252.219:3001/verifyCreds`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -100,7 +100,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
       // set userexist bool back to true (default)
       userExistBool = true
       console.log("creating new user...")
-      await fetch('http://localhost:3001/createUser', {
+      await fetch('http://18.218.252.219:3001/createUser', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -117,7 +117,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
   async function saveRecipes(username, recipe_id_string) {
 
     console.log("saving recipe...")
-    await fetch('http://localhost:3001/saveRecipes', {
+    await fetch('http://18.218.252.219:3001/saveRecipes', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -133,7 +133,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
   async function getRecipes(username) {
     var recipe_id_string = "null"
 
-    await fetch('http://localhost:3001/getRecipes', {
+    await fetch('http://18.218.252.219:3001/getRecipes', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -155,7 +155,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
 
     // save the diet
     console.log("saving diet...")
-    await fetch('http://localhost:3001/saveDiet', {
+    await fetch('http://18.218.252.219:3001/saveDiet', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -172,7 +172,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
   async function getUserDiet(username) {
     var this_diet = "null"
 
-    await fetch('http://localhost:3001/getUserDiet', {
+    await fetch('http://18.218.252.219:3001/getUserDiet', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -192,7 +192,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
   function saveAllergies(username, allergy_array) {
     console.log(username)
     console.log(allergy_array)
-    fetch('http://localhost:3001/saveAllergies', {
+    fetch('http://18.218.252.219:3001/saveAllergies', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -210,7 +210,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
     var allergy_array = []
     var allergy_array_string = ""
 
-    await fetch('http://localhost:3001/getAllergies', {
+    await fetch('http://18.218.252.219:3001/getAllergies', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -230,7 +230,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
   }
 
   function saveSchedule(username, schedule_string) {
-    fetch('http://localhost:3001/saveSchedule', {
+    fetch('http://18.218.252.219:3001/saveSchedule', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -248,7 +248,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
     var schedule_array = []
     var schedule_array_string = ""
 
-    await fetch('http://localhost:3001/getSchedule', {
+    await fetch('http://18.218.252.219:3001/getSchedule', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -273,7 +273,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
     console.log("saving bmi info")
     console.log(username + " " + weight + " " + feet + " " + inches + " " + age + " " + sex)
 
-    await fetch('http://localhost:3001/saveBMI', {
+    await fetch('http://18.218.252.219:3001/saveBMI', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -290,7 +290,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
   async function getWeight(username) {
     var this_weight = "null"
 
-    await fetch('http://localhost:3001/getWeight', {
+    await fetch('http://18.218.252.219:3001/getWeight', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -310,7 +310,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
   async function getFeet(username) {
     var this_feet = "null"
 
-    await fetch('http://localhost:3001/getFeet', {
+    await fetch('http://18.218.252.219:3001/getFeet', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -330,7 +330,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
   async function getInches(username) {
     var this_inches = "null"
 
-    await fetch('http://localhost:3001/getInches', {
+    await fetch('http://18.218.252.219:3001/getInches', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -350,7 +350,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
   async function getAge(username) {
     var this_age = "null"
 
-    await fetch('http://localhost:3001/getAge', {
+    await fetch('http://18.218.252.219:3001/getAge', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -370,7 +370,7 @@ function DatabaseHandler(functionName, username, password, diet, allergy_array, 
   async function getSex(username) {
     var this_sex = "null"
 
-    await fetch('http://localhost:3001/getSex', {
+    await fetch('http://18.218.252.219:3001/getSex', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
